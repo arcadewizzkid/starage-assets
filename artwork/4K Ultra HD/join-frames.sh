733b3b46b8bd531bce4f2d846b2082ca089e6b25
Joseph Granieri <info@arcadewizzkid.com>
2025-12-09 13:57:04 +1100

    station artwork

--- artwork/4K Ultra HD/join-frames.sh
+++ artwork/4K Ultra HD/join-frames.sh
@@ -16,13 +16,13 @@ montage output/cruiser*.png -tile 12x12  -geometry +0+0  joined/cruiser.png
 montage output/cship*.png -tile 12x12  -geometry +0+0  joined/cship.png
 
 montage output/gyro*.png -tile 10x10  -geometry +0+0  joined/gyro.png
+montage output/station*.png -tile 9x9  -geometry +0+0  joined/station.png
 
 montage output/powerup*.png -tile 8x8  -geometry +0+0  joined/powerup.png
 
 montage output/time-warp*.png -tile 16x16  -geometry +0+0  joined/time-warp.png
 
 montage output/shield*.png -tile 1x1  -geometry +0+0  joined/shield.png
-montage output/station*.png -tile 6x6  -geometry +0+0  joined/station.png
 
 montage output/button*.png -tile 2x2  -geometry +0+0  joined/button.png
 montage output/fire-button*.png -tile 2x2  -geometry +0+0  joined/fire-button.png
