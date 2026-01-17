3f3ac39505ee0faa804e1e98e9bf4c69c7b4ef15
Joseph Granieri <info@arcadewizzkid.com>
2026-01-17 20:58:06 +1100

    Towers

---
+++ artwork/towers/4K Ultra HD/join-frames.sh
@@ -0,0 +1,9 @@
+
+mkdir joined
+
+montage output/emp-tower*.png -tile 9x9  -geometry +0+0  joined/emp-tower.png
+montage output/rocket-tower*.png -tile 9x9  -geometry +0+0  joined/rocket-tower.png
+montage output/singularity*.png -tile 9x9  -geometry +0+0  joined/singularity.png
+montage output/wormhole*.png -tile 9x9  -geometry +0+0  joined/wormhole.png
+montage output/gun-tower*.png -tile 12x12  -geometry +0+0  joined/gun-tower.png
+montage output/laser-tower*.png -tile 12x12  -geometry +0+0  joined/laser-tower.png
